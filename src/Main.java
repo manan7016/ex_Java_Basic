9ef6d7441235a7d0bf3d619ab4fae674fcae3f55
Manan <manan7055@gmail.com>
2025-06-07 13:19:30 +0530

    second commit

---
+++ src/Main.java
@@ -0,0 +1,18 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello World");
+        System.out.println();
+        int rows = 5;
+        for (int i = 1; i <= rows; i++) {
+            for (int j = 1; j <= i; j++) {
+                System.out.print("* ");
+            }
+            System.out.println();
+        }
+        System.out.println();
+        System.out.println("Thank You!");
+    }
+}
